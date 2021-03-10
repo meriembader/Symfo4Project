db470e8a26a44441dad78c7a1acb88d78755855e
Meriem BADER <meriembader8@gmail.com>
2021-03-10 03:53:02 +0100

    not yet

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Dompdf\Dompdf;
 use Dompdf\Options;
+use Knp\Component\Pager\PaginatorInterface;
 /**
  * @Route("/user")
  */
@@ -18,12 +19,17 @@ class UserController extends AbstractController
     /**
      * @Route("/", name="user_index", methods={"GET"})
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $users = $this->getDoctrine()
             ->getRepository(User::class)
             ->findAll();
 
+        $user  = $this->get('knp_paginator')->paginate(
+            $users,
+            $request->query->get('page', 1)/*le numéro de la page à afficher*/,
+            4/*nbre d'éléments par page*/
+        );
         return $this->render('user/index.html.twig', [
             'users' => $users,
         ]);
